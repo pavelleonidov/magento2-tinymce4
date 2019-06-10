7383352775bca37f285893a2f4b3672e9ad4c6c4
Pavel Leonidov <pavel.leonidov@exconcept.com>
2019-06-10 12:02:45 +0200

    [TASK] Applied layout overrides for storefront side

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+	paths: {
+		"tinymce": "PavelLeonidov_TinyMce4/lib/tinymce4/js/tinymce.full.min",
+		"mage/adminhtml/wysiwyg/tiny_mce/setup": "PavelLeonidov_TinyMce4/mage/adminhtml/wysiwyg/tiny_mce/setup",
+		"mage/adminhtml/wysiwyg/widget": "PavelLeonidov_TinyMce4/mage/adminhtml/wysiwyg/widget"
+	}
+};
